a089628e870c82c8459403fd813bc4a18f910186
Anisha Jain <anishajain@outlook.com>
2019-09-29 01:39:32 -0700

    nav item web component

--- webComponents/navbar.js
+++ webComponents/navbar.js
@@ -10,17 +10,23 @@ import './navitem.js';
         this._shadowRoot = this.attachShadow({ mode: 'open' });
 
         this._navEl = document.createElement('div')
-        this._shadowRoot.appendChild(this._navEl)
+        // append ul element to this._navEl
+        // append ul styles
+        this._navUl = document.createElement('ul')
+
         this._centerAlign = false
-        this._bgColor = 'white'
+        this._color = 'white'
         this._data = [] // store tuples (nav item name, link)
+        this._curve = '' // bottom, top
 
         this.render()
+
+        this._shadowRoot.appendChild(this._navEl)
       }
   
       // Defines the attributes accessible to JS
       static get observedAttributes() {
-        return ['centerAlign', 'color','data'] // List an array of attribute names
+        return ['centerAlign', 'color','data', 'curve'] // List an array of attribute names
       }
 
       // append styles to item
@@ -31,30 +37,62 @@ import './navitem.js';
         this._navEl.style.flexDirection = 'row';
         this._navEl.style.justifyContent = 'space-between';
         this._navEl.style.alignItems = 'center';
-        this._navEl.style.backgroundColor = this._bgColor;
+        this._navEl.style.backgroundColor = this._color;
         this._navEl.style.alignItems = 'baseline';
         this._navEl.style.borderRadius = '10px';
-        this._navEl.style.color = LightenDarkenColor(this._bgColor, 20);
+        this._navEl.style.color = LightenDarkenColor(this._color, 20);
         // Align center styles
         if (this._centerAlign === true) {
             this._navEl.style.justifyContent = 'center !important';
         }
-      }
-      
-      // Parse the json string in data to get tuple (nav name, link)
-      _parseData() {
 
+        if (this._curve === 'bottom') {
+            this._navEl.style.height = '100px !important';
+            this._navEl.style.width = '100% !important';
+            this._navEl.style.borderBottomLeftRadius = '800% !important';
+            this._navEl.style.borderBottomRightERdius = '800% !important';
+            this._navEl.style.alignItems = flex-start;
+        }
+
+        if (this._curve === 'top') {
+            this._navEl.style.height = '100px !important';
+            this._navEl.style.width = '100% !important';
+            this._navEl.style.borderTopLeftRadius = '800% !important';
+            this._navEl.style.borderTopRightRadius = '800% !important';
+            this._navEl.style.alignItems = 'flex-end !important';
+        }
+      }
+
+      _styleUl() {
+        this._navUl.style.listStyleType = 'none';
+        this._navUl.style.padding = '0';
+        this._navUl.style.margin = '0';
+        this._navUl.style.display = 'flex';
+        this._navUl.style.flexDirection = 'row';
+        this._navUl.style.alignItems = 'baseline';
+        this._navUl.style.padding = '0px 20px';
       }
 
       // Handle changes to an attribute
       attributeChangedCallback(attributeName, oldValue, newValue) {
-        // use switch() {case: code, break} 
-        if (attributeName === 'centerAlign') {
-          // add styles for centering navbar elements
+        switch (attributeName) {
+            case 'data':
+                // TODO: remove attr.
+                this._data = JSON.parse(newValue)
+                break
+            case 'centerAlign':
+                this._centerAlign = newValue.toLowerCase() == 'true'
+                break
+            case 'color':
+                this._color = newValue
+                break
+            case 'curve':
+                this._curve = newValue.toLowerCase()
+                break
         }
       }
   
-      // Lifecycle method called when this component is appeded to the DOM
+      // Lifecycle method called when this component is appended to the DOM
       connectedCallback() {
         // Do things when component is added to the DOM
       }
@@ -62,7 +100,7 @@ import './navitem.js';
       // User defined method to 'render' this component.
       render() {
         this._styleNav()
-        this._parseData()
+        this._styleUl()
       }
     }
   
--- webComponents/navitem.js
+++ webComponents/navitem.js
@@ -1,3 +1,4 @@
+import { thisTypeAnnotation } from "babel-types";
 
 // Start with an anonymous function
 
@@ -9,16 +10,52 @@
         super(); // MUST call super!
         // Attach a shadow root to the element.
         this._shadowRoot = this.attachShadow({ mode: 'open' });
+        this._navItemEl = document.createElement('ul')
+        
+        this._title = ''
+        this._src = ''
+        this._logo = false // does this need logo styles
+        this._shadowRoot.appendChild(this._navItemEl)
+
+        this._listEl = document.createElement('li')
+        if (this._logo == true) {
+            // append h1 tag instead of (or inside??) the 'a' tag
+        }
+        this._listLinkEl = document.createElement('a')
+
+        this.render()
       }
-  
+      
+      _style() {
+        this._listLinkEl.style.display = 'block';
+        this._listLinkEl.style.padding = '1em';
+        this._listLinkEl.style.textDecoration = 'none';
+        this._listLinkEl.style.fontWeight = '600';
+        this._listLinkEl.onmouseover = () => {
+            this._listLinkEl.style.textDecoration = 'underline';
+        }
+        
+      }
+
       // Defines the attributes accessible to JS
       static get observedAttributes() {
-        return [] // List an array of attribute names
+        return ['title', 'src', 'logo'] // List an array of attribute names
       }
   
       // Handle changes to an attribute
       attributeChangedCallback(attributeName, oldValue, newValue) {
         // use switch() {case: code, break} 
+        switch(attributeName) {
+            case 'title':
+                this._title = newValue
+                break
+            case 'src':
+                this._src = newValue
+                break
+            case 'logo':
+                this._logo = newValue.toLowerCase()  == 'true'
+                break
+        }
         if (attributeName === '???') {
           // handle changes to an attribute
   
@@ -28,6 +65,10 @@
       // Lifecycle method called when this component is appeded to the DOM
       connectedCallback() {
         // Do things when component is added to the DOM
+      }
+
+      render() {
+        this._style()
       }
     }
   
